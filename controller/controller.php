d9d8d18254a4193318050f81d0f6b1467c09a483
Anderson Lima <andersoncardosolima0@gmail.com>
2016-09-02 17:19:58 -0300

    Inicieie a montagem da view, espero que os arquivos estejam organizados corrteamente dentro do padrão mvc

---
+++ controller/controller.php
@@ -0,0 +1,4 @@
+<link href='webroot/css/bootstrap.min.css' rel="stylesheet">
+<script src='webroot/js/bootstrap.min.js'></script>
+
+<?php require_once('view/leitor_codigo.php');?>
---
+++ view/leitor_codigo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Leitura de Codigo de Barras</title>
+</head>
+
+<style type="text/css">
+	.jumbotron{border: 1px solid #999; box-shadow: -0px -0px 45px #999;}
+</style>
+
+<body>
+
+<div class="container">
+
+	<br>
+	<div class="jumbotron" style="background-image: url('webroot/images/logo_fatec.jpg');">
+		<form>
+			<div class="row">
+				<center class="col-md-6 col-md-offset-3 col-xs-6 col-xs-offset-3">
+					<!-- colocar aqui talvez o codigo de barra gerado pelo numero no input -->
+					<h1>|||||||||||||||</h1>
+				</center>
+			</div>
+
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 col-xs-6 col-xs-offset-3">
+					<center><b>Numero do Código de Barra</b></center>
+					<input type="text" name="codigo_barra" id="codigo_barra" class="form-control">	
+				</div>
+			</div>
+
+		</form>
+	</div>
+</div>
+
+</body>
+</html>
