b54a6d37f4622e69ba6962c402f7ef98321780d6
Marlon <marlu_tcu@hotmail.com>
2016-09-29 00:57:17 -0300

    consegui fazer a grid sumir na impressão porem ficou apenas com 1 coluna, precisa de ajutes

--- model/geraetiqueta.php
+++ model/geraetiqueta.php
@@ -1,10 +1,12 @@
 	
-
+<!DOCTYPE>
 <head>
 	<meta charset="utf-8">
 	<title></title>
 	<script type="text/javascript" src="../webroot/js/jquery-2.1.1.min.js"></script>
 	<script type="text/javascript" src="../webroot/js/jquery-ui.js"></script>
+	<link rel="stylesheet" type="text/css" href="../webroot/css/style.css" >
+	<link rel="stylesheet" type="text/css" href="../webroot/css/print.css" media="print" >
 	
 </head> 
 
@@ -21,30 +23,33 @@ geraCodigoBarra($etiqueta[0]['cod_bar']);
 echo '<br>'.$etiqueta[0]['cod_bar'];
 echo '</center>';
 echo'</div>';
-?>
 
-
-<?php 
-for($x=1;$x<34;$x++){
-	echo '<div class="view" style="display:inline-block;height:90px;width:234px;border: solid black ;padding:0px;margin:2px"></div>';
+for($x=1;$x<33;$x++){
+	echo '<div class="view" style="height:90px;width:234px;border:2px solid black ;padding:0px;margin:2px">';
+	
+	echo '</div>';
 
 }
+
 ?>
+
+
+
 
 <STYLE TYPE="text/css">
 
-@media print{
-    .iew{display:none;}
-}
+
 	p{
 		text-align: center;
 		margin: 0px;
 		padding: 0px;
 	}
 	body{
+		display: block;
 		margin:0px;
 		padding: 0px;
 		width: 793.700px;
+		
 
 	}
 </STYLE>
