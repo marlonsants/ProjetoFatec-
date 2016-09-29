ef383f9f8e269101129256831e1ecd4f4f5db2ee
Anderson Lima <andersoncardosolima0@gmail.com>
2016-09-29 14:03:36 -0300

    Adicionei um efeito no menu

--- view/menu.php
+++ view/menu.php
@@ -1,22 +1,29 @@
+<!--style type="text/css">
+
+
+  <center class="col-md-3">
+    <h4><a href="presenca.php" id="meulink"></a></h4>
+  </center>
+</nav-->
+
 <style type="text/css">
   #meulink{color: rgb(50,180,74); text-decoration:none}
-  #meulink:hover{color: green; text-decoration:none}
+  #meulink:hover{color: white; text-decoration:none; background-color: rgb(50,180,74);}
 </style>
-
 <nav>
-  <center class="col-md-3">
-    <h4><a href="leitor_codigo.php" id="meulink"><span class="glyphicon glyphicon-barcode" ></span> Verificar Cracha</a></h4>
-  </center>
+  <a href="leitor_codigo.php" id="meulink" class="col-md-3">
+    <center><h4><span class="glyphicon glyphicon-barcode" ></span> Verificar Cracha</h4></center>
+  </a>
 
-  <center class="col-md-3">
-    <h4><a href="sorteio.php" id="meulink"><span class="glyphicon glyphicon-stats" ></span> Sorteio</a></h4>
-  </center>
+  <a href="sorteio.php" id="meulink" class="col-md-3">
+    <center><h4><span class="glyphicon glyphicon-stats" ></span> Sorteio</h4></center>
+  </a>
 
-  <center class="col-md-3">
-    <h4><a href="cadastro.php" id="meulink"><span class="glyphicon glyphicon-pencil" ></span> Cadastro</a></h4>
-  </center>
+  <a href="cadastro.php" id="meulink" class="col-md-3">
+    <center><h4><span class="glyphicon glyphicon-pencil" ></span> Cadastro</h4></center>
+  </a>
 
-  <center class="col-md-3">
-    <h4><a href="presenca.php" id="meulink"><span class="glyphicon glyphicon-calendar" ></span> Presença</a></h4>
-  </center>
+  <a href="presenca.php" id="meulink" class="col-md-3">
+    <center><h4><span class="glyphicon glyphicon-calendar" ></span> Presença</h4></center>
+  </a>
 </nav>
