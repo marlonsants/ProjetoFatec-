b09469e0246443e02c0f2c1af3a643583cf3bf6f
Anderson Lima <andersoncardosolima0@gmail.com>
2016-09-29 14:11:25 -0300

    Retirei os comentarios

--- view/menu.php
+++ view/menu.php
@@ -1,29 +1,22 @@
-<!--style type="text/css">
-
-
-  <center class="col-md-3">
-    <h4><a href="presenca.php" id="meulink"></a></h4>
-  </center>
-</nav-->
-
 <style type="text/css">
   #meulink{color: rgb(50,180,74); text-decoration:none}
   #meulink:hover{color: white; text-decoration:none; background-color: rgb(50,180,74);}
 </style>
+
 <nav>
   <a href="leitor_codigo.php" id="meulink" class="col-md-3">
-    <center><h4><span class="glyphicon glyphicon-barcode" ></span> Verificar Cracha</h4></center>
+    <center><h4><span class="glyphicon glyphicon-barcode"></span> Verificar Cracha</h4></center>
   </a>
 
   <a href="sorteio.php" id="meulink" class="col-md-3">
-    <center><h4><span class="glyphicon glyphicon-stats" ></span> Sorteio</h4></center>
+    <center><h4><span class="glyphicon glyphicon-stats"></span> Sorteio</h4></center>
   </a>
 
   <a href="cadastro.php" id="meulink" class="col-md-3">
-    <center><h4><span class="glyphicon glyphicon-pencil" ></span> Cadastro</h4></center>
+    <center><h4><span class="glyphicon glyphicon-pencil"></span> Cadastro</h4></center>
   </a>
 
   <a href="presenca.php" id="meulink" class="col-md-3">
-    <center><h4><span class="glyphicon glyphicon-calendar" ></span> Presença</h4></center>
+    <center><h4><span class="glyphicon glyphicon-calendar"></span> Presença</h4></center>
   </a>
 </nav>
