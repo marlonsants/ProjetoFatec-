33b0526dd331d2ffc7575f15b59c4f990a3e1dc0
Anderson Lima <andersoncardosolima0@gmail.com>
2016-09-30 15:02:23 -0300

    corrigindo um pequeno erro da sessão

--- sessao/sessao.php
+++ sessao/sessao.php
@@ -11,7 +11,7 @@ require_once('../config/funcoesmysql.php');
 		$_SESSION['logado'] = true;
 		$_SESSION['usuario'] = $usuario;
 		$_SESSION['senha'] = $senha;
-		header('location: ../view/leitor_codigo');
+		header('location: ../view/leitor_codigo.php');
 	}else{
 		header('location: ../index.php?erro=1');
 	}
