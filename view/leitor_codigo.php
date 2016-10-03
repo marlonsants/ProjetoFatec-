74cea57fd8016026b57ca830bfd53ff5f953a097
Anderson Lima <littletape@gmail.com>
2016-10-03 20:55:10 -0300

    ajustei as mensagens

--- view/leitor_codigo.php
+++ view/leitor_codigo.php
@@ -44,8 +44,10 @@
 					<div class="col-md-12 col-xs-12" id="borda" style=" border-color: rgb(40,145,58);"></div>
 
 					<div class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1" id="info">
-						<h3><b>Nome:</b><span id="nome"></span> </h3>
-						<h3><b>Status: </b><span id="confirma"></span></h3>
+						<h3 class="col-md-12 col-xs-12"><b>Nome:</b><span id="nome"></span> </h3>
+
+						<h3 class="col-md-2 col-xs-2"><b>Status: </b></h3>
+						<h3 class="col-md-10 col-xs-10" id="confirma"></h3>
 					</div>
 
 					<div class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1">
